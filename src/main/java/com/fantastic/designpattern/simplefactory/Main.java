b7e0697f2de4c164858f45d7f1e5cd45a968d3eb
companycomputer <544520418@qq.com>
2018-07-30 18:37:12 +0800

    init

---
+++ src/main/java/com/fantastic/designpattern/simplefactory/Main.java
@@ -0,0 +1,16 @@
+package com.fantastic.designpattern.simplefactory;
+
+/**
+ * 简单工厂模式
+ *  [功能详细描述]
+ * @作者 lwqMR
+ * @version [版本号, 2018年7月30日]
+ * @see [相关类/方法]
+ * @since [产品/模块版本] 
+ */
+public class Main
+{
+    public static void main(String[] args) {
+        
+    }
+}
